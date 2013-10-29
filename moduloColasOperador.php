4666e4e348db08c446519157ce15510b62c8b072
Jose fuentes <jose.fuentes@pangeatech.com.ve>
2013-10-29 17:53:47 -0430

    modulocolaOperador completo

--- moduloColasOperador.php
+++ moduloColasOperador.php
@@ -1,22 +1,18 @@
 <?php 
   require_once('nusoap.php'); 
-  $wsdl_url = 'http://localhost:15362/HoriFarmacia/Joseweb?WSDL';
+  $wsdl_url = 'http://localhost:15362/HoriFarmacia/ColaWS?WSDL';
   $client = new SOAPClient($wsdl_url);
   $client->decode_utf8 = false; 
-  $Analista= array('idAnalista' => '1');
-  $Resultado = $client->listaPreordenMedicamentoAnalistaXidAnalista($Analista);
- $i=0;
-  foreach ($Resultado->return as $r) {
-	  $i++;
-  }
-  echo '__________';
- //print_r($Resultado->return);
-  print_r(count($Resultado->return));
+  $Analista = array('idAnalista' => '1');
+  $Resultado = $client->listaPreordenAnalistaXidAnalista($Analista);
   $reg=count($Resultado->return);
- echo '__________°°°°°°°°°°°°°°°°°°°°°°°°°°°°______________';
- $Resultad2 = $client->promedioSolicitudesXidAnalista($Analista);
-  $pro=count($Resultado2->return);
-
+  $Resultad2 = $client->promedioSolicitudesXidAnalista($Analista);
+  $promedio =$Resultad2->return;
+  $estadoCon= array('estado' =>'1');
+  $Resultad3 = $client->obtenerTotalOperadoresConectadosXEstado($estadoCon);
+  $conectados = $Resultad3->return;
+  
+  
 ?>
 
 <!DOCTYPE html>
@@ -102,7 +98,7 @@
 			             
               <div class="tab-content">
                   <div class="span10">
-                        <button type="button" class="btn btn-success">Regresar</button>
+                        <button type="button" class="btn btn-success"><i class="icon-arrow-left"></i> Regresar<a href="moduloColasVisualizar.php"></a></button>
                   </div>
                         
                   <div class="span10"> 
@@ -112,7 +108,12 @@
                               <tbody>
                                 <tr>
                                   <th align="left"> Nombre de Operador:</th>
-                                  <td align="right">Jhon</td>
+                                  <td align="right"><?php 
+								                if($reg>1){
+													echo $Resultado->return[0]->idanalista->nombre; 
+													} else{ 
+													echo $Resultado->return->idanalista->nombre; ; }?>
+                                                    </td>
                                 </tr>
                              
                                 <tr>
@@ -122,12 +123,12 @@
                              
                                 <tr>
                                   <th align="left">Promedio de Solicitudes atendidas:</th>
-                                  <td align="right"><?php echo $pro?></td>
+                                  <td align="right"><?php echo $promedio?></td>
                                 </tr>
                              
                                 <tr>
                                   <th align="left">Operadores Conectados</th>
-                                  <td align="right">5</td>
+                                  <td align="right"><?php echo $conectados?></td>
                                 </tr>
                               </tbody>
                             </table>
@@ -148,16 +149,59 @@
 							  </thead>
                               <tbody>
                                 <tr>
+                                
                                 <?php
+								if($reg!=0){
+									
+									
+								if($reg>1){
 								  $j=0;
 								     while($j<$reg){ ?>
-               <td align="center">    <?php echo $Resultado->return[0]->preordMedId->idpreorden->idpreorden ?></td>
-                                  <td align="center">  <?php echo $Resultado->return[0]->fecha ?></td>
-                                  <td align="center"><?php echo $Resultado->return[0]->preordMedId->idpreorden->aprobado ?></td>
-                                   <td align="center"><button type="button" class="btn btn-success"><i class="icon-check"></i></button></td>
+                                  <td align="center"> <?php echo $Resultado->return[$j]->preorden->idpreorden ?></td>
+                                  <td align="center">  <?php echo $Resultado->return[$j]->fecha ?></td>
+                                  <td align="center"><?php 
+								  if($Resultado->return[$j]->preorden->aprobado=="A"){
+									  echo "Aprobada";
+								  }
+								  if($Resultado->return[$j]->preorden->aprobado=="D"){
+									  echo "Denegada";
+								  }
+								  if($Resultado->return[$j]->preorden->aprobado=="P"){
+									  echo "Aprobada Parcial";
+								  }
+								  ?></td>
+                                  </td><td style="text-align:center"><a href="moduloColasAuditoria.html?id=<?php echo $id?>"><i class="icon-check"></i></a></td>
+                                   
+                                   
                                 </tr>
                            
-                               <?php $j++; } ?>
+                           
+                               <?php
+									 $j++;
+									 } 
+									 }else{  ?>
+								  <td align="center">    <?php echo $Resultado->return->preorden->idpreorden ?></td>
+                                  <td align="center">  <?php echo $Resultado->return->fecha ?></td>
+                                  <td align="center"><?php 
+								  if($Resultado->return->preorden->aprobado=="A"){
+									  echo "Aprobada";
+								  }
+								  if($Resultado->return->preorden->aprobado=="D"){
+									  echo "Denegada";
+								  }
+								  if($Resultado->return->preorden->aprobado=="P"){
+									  echo "Aprobada Parcial";
+								  }?>							
+                                  </td><td style="text-align:center"><a href="moduloColasAuditoria.php?id=<?php echo $Resultado->return->preorden->idpreorden ?>"><i class="icon-check"></i></a></td>
+                                   
+                                   
+                                </tr>
+								<?php		 
+									 }
+								   }   else {
+										 
+									 }
+							     ?>
                                  
                               </tbody>
                             </table>
